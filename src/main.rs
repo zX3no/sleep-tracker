b393067d578eca8f063f64f7890202e3a309ed80
zX3no <dr_xeno@hotmail.com.au>
2021-08-28 17:42:34 +0930

    formatting

--- src/main.rs
+++ src/main.rs
@@ -15,26 +15,82 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    let datasets = vec![Dataset::default()
-        .name("data1")
-        .marker(symbols::Marker::Dot)
-        .graph_type(GraphType::Scatter)
-        .style(Style::default().fg(Color::Magenta))
-        .data(&[(1.0, 8.0), (2.0, 9.0), (3.0, 9.0), (4.0, 7.0), (5.0, 8.0)])];
+    let datasets = vec![
+        Dataset::default()
+            .name("August")
+            .marker(symbols::Marker::Braille)
+            .graph_type(GraphType::Line)
+            .style(Style::default().fg(Color::Magenta))
+            .data(&[
+                (1.0, 8.0),
+                (2.0, 9.0),
+                (3.0, 9.0),
+                (4.0, 7.0),
+                (5.0, 12.0),
+                (6.0, 8.0),
+                (7.0, 9.0),
+                (8.0, 10.0),
+                (9.0, 7.0),
+                (10.0, 8.0),
+                (11.0, 6.0),
+                (12.0, 8.0),
+                (13.0, 12.0),
+                (14.0, 9.0),
+                (15.0, 7.0),
+                (16.0, 6.0),
+                (17.0, 9.0),
+                (18.0, 13.0),
+                (19.0, 10.0),
+                (20.0, 5.0),
+                (21.0, 9.0),
+                (22.0, 7.0),
+                (23.0, 9.0),
+            ]),
+        Dataset::default()
+            .name("July")
+            .marker(symbols::Marker::Braille)
+            .graph_type(GraphType::Line)
+            .style(Style::default().fg(Color::Cyan))
+            .data(&[
+                (1.0, 9.0),
+                (2.0, 7.0),
+                (3.0, 8.0),
+                (4.0, 9.0),
+                (5.0, 11.0),
+                (6.0, 8.0),
+                (7.0, 2.0),
+                (8.0, 10.0),
+                (9.0, 7.0),
+                (10.0, 6.0),
+                (11.0, 6.0),
+                (12.0, 5.0),
+                (13.0, 10.0),
+                (14.0, 8.0),
+                (15.0, 9.0),
+                (16.0, 7.0),
+                (17.0, 8.0),
+                (18.0, 9.0),
+                (19.0, 4.0),
+                (20.0, 6.0),
+                (21.0, 8.0),
+                (22.0, 9.0),
+                (23.0, 7.0),
+            ]),
+    ];
 
     terminal.clear()?;
 
     terminal.draw(|f| {
-        let area = Rect::new(0, 0, 100, 40);
+        let area = f.size();
         let chart = Chart::new(datasets)
             .block(Block::default().title("Chart"))
             .x_axis(
                 Axis::default()
-                    .title(Span::styled("Date", Style::default().fg(Color::Red)))
+                    .title(Span::styled("Date", Style::default().fg(Color::Cyan)))
                     .style(Style::default().fg(Color::White))
-                    .bounds([0.0, 10.0])
+                    .bounds([0.0, 30.0])
                     .labels(
-                        ["0.0", "5.0", "10.0"]
+                        ["0", "10", "20", "30"]
                             .iter()
                             .cloned()
                             .map(Span::from)
@@ -43,11 +99,14 @@ fn main() -> Result<(), io::Error> {
             )
             .y_axis(
                 Axis::default()
-                    .title(Span::styled(" Time Slept", Style::default().fg(Color::Red)))
+                    .title(Span::styled(
+                        " Time Slept",
+                        Style::default().fg(Color::Cyan),
+                    ))
                     .style(Style::default().fg(Color::White))
-                    .bounds([0.0, 14.0])
+                    .bounds([0.0, 16.0])
                     .labels(
-                        ["0.0", "7.0", "14.0"]
+                        ["0", "4", "8", "12", "16"]
                             .iter()
                             .cloned()
                             .map(Span::from)
